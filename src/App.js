4cb2e1d4f18499483caa2be09236c1937c8f3794
ananthomprakash <ananthomprakash@gmail.com>
2025-02-03 01:22:17 +0530

    Added props and passed props to component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+function Greetings(props) {
+  return (
+    <div>
+      <p>
+        Hello, {props.name}! You are {props.age} years old and you are from{" "}
+        {props.city}.
+      </p>
+    </div>
+  );
+}
 function App() {
   const name = "Test";
   const age = 30;
@@ -9,9 +19,7 @@ function App() {
     <div>
       <h1> Welcome to React</h1>
       <p> This is my first React app</p>
-      <p>
-        My name is {name} and i am {age} years old and i am from {city}.
-      </p>
+      <Greetings name={name} age={age} city={city} />
     </div>
   );
 }
